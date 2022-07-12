f5f69c0c767a11f2c246341af13259390de48c54
rahadiana nugraha <rahadiana@users.noreply.github.com>
2022-07-12 20:36:38 +0700

    Update FindQuery.js

--- helpers/FindQuery.js
+++ helpers/FindQuery.js
@@ -1,6 +1,6 @@
 const GetQuery = function(query) {
     let BeforeText = '<body id="content" style="margin-top: 56px;">';
-    let AfterText = '<div id="goog-gt-tt"';
+    let AfterText = query.includes('<div id="goog-gt-tt"') == true ? '<div id="goog-gt-tt"' :'</body>';
     let Text = ' ' + query;
     let This = Text.indexOf(BeforeText);
     if (This == 0) return '';
